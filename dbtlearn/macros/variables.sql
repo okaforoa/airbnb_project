7f94e42357b6d876b3618721e813f287a647627f
Dion Okafor <49908077+okaforoa@users.noreply.github.com>
2025-05-05 09:27:56 -0400

    Initial commit: Airbnb project with dbt and dagster

---
+++ dbtlearn/macros/variables.sql
@@ -0,0 +1,7 @@
+{% macro learn_variables() %}
+
+    {% set your_name_jinja = "Dion" %}
+    {{ log("Hello " ~ your_name_jinja, info=True)}}
+
+    {{ log("Hello dbt user " ~ var("user_name", "NO USERNAME IS SET!!") ~ "!", info=True) }}
+{% endmacro %}
---
+++ dbtlearn/models/overview.md
@@ -0,0 +1,11 @@
+{% docs __overview__ %}
+
+# Airbnb Pipeline
+
+Hey, welcome to our Airbnb pipeline documentation!
+
+Here is the schema of our input data:
+
+![Input Schema](assets/input_schema.png)
+
+{% enddocs %}
